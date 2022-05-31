d94e808048a55df6c6cdbbc8095d291851c50b95
Regina <reginaoliveira.rmd@gmail.com>
2022-05-30 21:37:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RPG.src.Entities;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero( "Arus", 23, "knight");
+            
+            Console.WriteLine(arus);
+            Console.WriteLine(arus.Attack());
+            
+        }
+    }
+}
